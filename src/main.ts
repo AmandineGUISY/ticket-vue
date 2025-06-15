63b39951ab7cfb946f3edc286e94cbfd5528411c
AmandineGUISY <aguisy@yahoo.fr>
2025-06-15 16:04:01 +0200

    ADD: Create App.vue component with router view for main application layout

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue';
-import App from './app/login.vue';
+import App from './App.vue';
 import router from './router/index.ts';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
