09461bd3b48796e9e51c9e9993f375ceef9e7b86
AmandineGUISY <aguisy@yahoo.fr>
2025-06-15 11:11:27 +0200

    INIT: Starting a Vue.js application with TypeScript, implementing a ticket creation system similar to GitHub.

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <div class="container mt-5">
+    <h1 class="text-primary">Hello Bootstrap avec Vue.js !</h1>
+    <button class="btn btn-success">Clique moi</button>
+  </div>
+</template>
+
