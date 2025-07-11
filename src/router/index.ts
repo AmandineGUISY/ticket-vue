a823aec2c1f689e168a9c1c2843516739c449faa
AmandineGUISY <aguisy@yahoo.fr>
2025-07-11 11:26:58 +0200

    FIX: update router to use createWebHashHistory for improved navigation

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import LoginView from '../app/login.vue';
 import RegisterView from '../app/register.vue';
 import CrmView from '../app/crm.vue';
@@ -28,7 +28,7 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(),
     routes,
 });
 
